fe6e6245414757a6b751b2870400c169190fcbcc
yunggenius <yunggenius4@gmail.com>
2019-09-27 23:04:26 +0100

    clean

--- realIndex.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction(){
-      var inputVal = document.getElementById('userInput').value;
-       document.getElementById('list').innerHTML= inputVal;
-}
-
-
-var items = []
-items.push(inputVal)
