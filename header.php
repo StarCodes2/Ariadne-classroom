d86760f1fd2272e92ae68882736682fa11f37b4e
yunggenius <yunggenius4@gmail.com>
2019-09-27 23:14:33 +0100

    php

---
+++ header.php
@@ -0,0 +1,11 @@
+    <div class="topnav" id="myTopnav">
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()"><img src="https://res.cloudinary.com/siyfa/image/upload/v1568922461/ovqrbsa6t7nhghflejve.png" style="width: 30px;">
+        </a>
+        <a href="sign-up.html">Login</a>
+        <a href="#">Contact Us</a>
+        <a href="#">FAQ</a>
+        <a href="#">Courses</a>
+        <a href="about-us.html">How it works</a>
+        <a href="class.html">Create Class</a>
+        <a href="#">Home</a>
+  </div>
